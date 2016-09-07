b68cfcae0ad5a9f13b47bbfb74871e6e143f5a1f
oleguchok <oleguchok@gmail.com>
2016-09-07 23:48:01 +0300

    Add family and user entities

---
+++ src/FamilyHelper.Entities/Entities/Family.cs
@@ -0,0 +1,10 @@
+﻿using FamilyHelper.Entities.Abstract;
+
+namespace FamilyHelper.Entities.Entities
+{
+    public class Family : IEntityBase
+    {
+        public int Id { get; set; }
+        public string FamilyName { get; set; }
+    }
+}
---
+++ src/FamilyHelper.Entities/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FamilyHelper.Entities.Abstract;
+
+namespace FamilyHelper.Entities.Entities
+{
+    public class User : IEntityBase
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string HashedPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
