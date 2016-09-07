e93aebcdbbe975f3be10edf73b3b74d23a9c1df7
oleguchok <oleguchok@gmail.com>
2016-09-08 01:42:14 +0300

    Add DbContext and RepositoryInterface

---
+++ src/FamilyHelper.Data/Abstract/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FamilyHelper.Entities.Abstract;
+
+namespace FamilyHelper.Data.Abstract
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+
+    }
+}
