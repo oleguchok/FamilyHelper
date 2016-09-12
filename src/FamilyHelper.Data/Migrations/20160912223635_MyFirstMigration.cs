33047b71be58790ba03a2f36b6638b00401607e1
oleguchok <oleguchok@gmail.com>
2016-09-13 01:48:25 +0300

    Create migrations

---
+++ src/FamilyHelper.Data/Migrations/20160912223635_MyFirstMigration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FamilyHelper.Data.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Families",
+                columns: table => new
+                {
+                    family_id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FamilyName = table.Column<string>(maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Families", x => x.family_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    FamilyId = table.Column<long>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    HashedPassword = table.Column<string>(maxLength: 450, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    UserName = table.Column<string>(maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Families_FamilyId",
+                        column: x => x.FamilyId,
+                        principalTable: "Families",
+                        principalColumn: "family_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Families_FamilyName",
+                table: "Families",
+                column: "FamilyName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_FamilyId",
+                table: "Users",
+                column: "FamilyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Families");
+        }
+    }
+}
---
+++ src/FamilyHelper.Data/Migrations/FamilyHelperContextModelSnapshot.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using FamilyHelper.Data;
+
+namespace FamilyHelper.Data.Migrations
+{
+    [DbContext(typeof(FamilyHelperContext))]
+    partial class FamilyHelperContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("FamilyHelper.Entities.Entities.Family", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("family_id");
+
+                    b.Property<string>("FamilyName")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 150);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FamilyName")
+                        .IsUnique();
+
+                    b.ToTable("Families");
+                });
+
+            modelBuilder.Entity("FamilyHelper.Entities.Entities.User", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateOfBirth");
+
+                    b.Property<string>("Email");
+
+                    b.Property<long>("FamilyId");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 100);
+
+                    b.Property<string>("HashedPassword")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 450);
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 100);
+
+                    b.Property<string>("UserName")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 150);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FamilyId");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("FamilyHelper.Entities.Entities.User", b =>
+                {
+                    b.HasOne("FamilyHelper.Entities.Entities.Family", "Family")
+                        .WithMany("Users")
+                        .HasForeignKey("FamilyId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
