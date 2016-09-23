66d3f748ecd9ddb8b6787e5265dc4c19c65de3aa
oleguchok <oleguchok@gmail.com>
2016-09-23 16:54:56 +0300

    Workaround for ef core migrations

---
+++ src/FamilyHelper.Data/TemporaryDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace FamilyHelper.Data
+{
+    public class TemporaryDbContextFactory : IDbContextFactory<FamilyHelperContext>
+    {
+        public FamilyHelperContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<FamilyHelperContext>();
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FamilyHelper;Trusted_Connection=True;");
+            return new FamilyHelperContext(builder.Options);
+        }
+    }
+}
