375bc2b92f98543e2685ca217e783b2d58cce02f
oleguchok <oleguchok@gmail.com>
2016-09-24 00:04:34 +0300

    Add login and register dto's

---
+++ src/FamilyHelper.API/Models/DTO/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FamilyHelper.API.Models.DTO
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/FamilyHelper.API/Models/DTO/UserRegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FamilyHelper.API.Models.DTO
+{
+    public class UserRegisterDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+    }
+}
