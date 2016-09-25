00602993efb0b86faef45ae9ccc8c5e3ee82f7ca
oleguchok <oleguchok@gmail.com>
2016-09-25 22:19:54 +0300

    Use static files. Add index.html

--- src/FamilyHelper.SPA/Startup.cs
+++ src/FamilyHelper.SPA/Startup.cs
@@ -28,10 +28,8 @@ namespace FamilyHelper.SPA
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
